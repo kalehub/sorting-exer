15ee1439abb3bb32ce23dcc80136260d346c22ba
kalehub <=>
2021-01-28 09:56:10 +0800

    bubble sort

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class CustomList:
     def __init__(self, mylist):
         self.mylist = mylist
 
-    def selection_sort(self):
+    def bubble_sort(self):
         """ find the minimum element and put it in front """
         print('before sorted : ', self.mylist)
 
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     # [8, 4, 6, 9, 2, 5, 3]
 
     clist = CustomList([64, 25, 12, 22, 11])
-    clist.selection_sort()
+    clist.bubble_sort()
