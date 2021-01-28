3a82eda96c9f87ef673d328a571d7e54ce094a47
kalehub <=>
2021-01-28 09:53:03 +0800

    added selection sort

--- main.py
+++ main.py
@@ -4,12 +4,22 @@
 class CustomList:
     def __init__(self, mylist):
         self.mylist = mylist
-    
+
     def selection_sort(self):
         """ find the minimum element and put it in front """
+        print('before sorted : ', self.mylist)
+
+        for i in range(0, len(self.mylist)):
+            for j in range(i+1, len(self.mylist)):
+                if self.mylist[i] > self.mylist[j]:
+                    self.mylist[i], self.mylist[j] = self.mylist[j], self.mylist[i]
+            print('iterasi  : ', self.mylist)
+            # swapping
+        print(self.mylist)
 
 
 if __name__ == '__main__':
-   clist = CustomList([8,4,6,9,2,5,3])
-   clist.selection_sort()
+    # [8, 4, 6, 9, 2, 5, 3]
 
+    clist = CustomList([64, 25, 12, 22, 11])
+    clist.selection_sort()
