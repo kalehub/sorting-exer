39b5bdb05fb4cfd6869c1f538b38e90e7e5de717
kalehub <=>
2021-01-28 10:16:38 +0800

    adding selection sort

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ class CustomList:
         self.mylist = mylist
 
     def bubble_sort(self):
-        """ find the minimum element and put it in front """
+        """ swap every element found """
+        print('\n--Bubble Sort')
         print('before sorted : ', self.mylist)
 
         for i in range(0, len(self.mylist)):
@@ -15,11 +16,25 @@ class CustomList:
                     self.mylist[i], self.mylist[j] = self.mylist[j], self.mylist[i]
             print('iterasi  : ', self.mylist)
             # swapping
-        print(self.mylist)
+        print('sorted : ', self.mylist)
+
+    def selection_sort(self):
+        """ find the minimum value and swap it to front """
+        print('\n--Selection Sort')
+        print('before sorted : ', self.mylist)
+
+        for i in range(0, len(self.mylist)):
+            min_index = i
+            for j in range(i+1, len(self.mylist)):
+                if self.mylist[min_index] > self.mylist[j]:
+                    min_index = j
+            self.mylist[i], self.mylist[min_index] = self.mylist[min_index], self.mylist[i]
+            print('proses : ', self.mylist)
+        print('sorted : ', self.mylist)
 
 
 if __name__ == '__main__':
-    # [8, 4, 6, 9, 2, 5, 3]
 
     clist = CustomList([64, 25, 12, 22, 11])
-    clist.bubble_sort()
+    # clist.bubble_sort()
+    clist.selection_sort()
