caa31705ab317420711ae30b90eab035e87212d8
kale <akun.mannequin@gmail.com>
2021-01-28 09:06:28 +0800

    adding the file

---
+++ main.py
@@ -0,0 +1,15 @@
+# selection sort
+# python
+
+class CustomList:
+    def __init__(self, mylist):
+        self.mylist = mylist
+    
+    def selection_sort(self):
+        """ find the minimum element and put it in front """
+
+
+if __name__ == '__main__':
+   clist = CustomList([8,4,6,9,2,5,3])
+   clist.selection_sort()
+
