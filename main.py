e75ab64faafbba56c9d2d5f72f74a411bf9ad08c
kale <akun.mannequin@gmail.com>
2021-01-29 09:58:29 +0800

    adding insetion sort

--- main.py
+++ main.py
@@ -31,10 +31,25 @@ class CustomList:
             self.mylist[i], self.mylist[min_index] = self.mylist[min_index], self.mylist[i]
             print('proses : ', self.mylist)
         print('sorted : ', self.mylist)
+    
+    def insertion_sort(self):
+        """ iterte the list, if it smaller than the predecessor compare it to the element before """
+        _list = self.mylist
+
+        for i in range(1, len(_list)):
+            key = _list[i]
+            j = i-1
+            while j >= 0 and key < _list[j]:
+                _list[j+1] = _list[j]
+                j-=1
+            _list[j+1] = key
+        
+        print(_list)
 
 
 if __name__ == '__main__':
 
     clist = CustomList([64, 25, 12, 22, 11])
     # clist.bubble_sort()
-    clist.selection_sort()
+    # clist.selection_sort()
+    clist.insertion_sort()
